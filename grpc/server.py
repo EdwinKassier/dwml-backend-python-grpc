ef71d36260168bfb65c40b0909b657bd88e541ad
Edwin Kassier <edwin@sweepsouth.com>
2023-10-19 15:16:50 +0200

    Move back to 50051

--- grpc/server.py
+++ grpc/server.py
@@ -12,7 +12,7 @@ def serve():
     print('Starting serve')
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb2_grpc.add_APIServicer_to_server(APIService(), server)
-    server.add_insecure_port('[::]:443')
+    server.add_insecure_port('[::]:50051', options=(('grpc.enable_http_proxy', 0),))
     print('About to serve port')
     server.start()
     server.wait_for_termination()
